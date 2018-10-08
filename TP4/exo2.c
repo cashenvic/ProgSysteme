31d37821bc2a09591c6d931c7916f69765b27b90
Drissa Cisse <cissed@master-nvme.e-im2ag.ujf-grenoble.fr>
2018-10-08 11:54:38 +0200

    Exo2 fini (ps ax|grep bash|wc - l)

--- TP4/exo2.c
+++ TP4/exo2.c
@@ -3,33 +3,56 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#define ECRITURE 1
+#define LECTURE 0
 
+void initPipe(int p[2], int valChanged) {
 
+	close(valChanged);
+	dup(p[valChanged]);
+	close(p[valChanged]);
+	close(p[!valChanged]);
+}
 int main(int argc, char const *argv[]) {
-	/*	
-		ps ax | grep bash
-	
-		ps ax | grep bash | wc -l
-	*/
 
-	//char *macommande[3] = { "ps", "ax", " | grep bash | wc -l", (char *)0 };
-	char *macommande[2] = { "ax", (char *)0 };
+	int p1[2], p2[2];
 
-	switch (fork()){
-		case -1 :
-			fprintf(stderr, "Erreur de fork\n");
-			exit(-1);
-			break;
-		case 0 :
-			if (execvp("ps", macommande)== -1) {
-				perror("execvp");
-				exit(-1);
-			}
-			break;
-		default :
-			printf("Le fils %d est mort\n", wait(NULL));
-			return 0;
-	}
+	pipe(p1);
+	pipe(p2);
 
+
+	switch(fork()){
+		case -1: 
+		 	perror("fork"); 
+			exit(1);
+		case 0:	
+
+			initPipe(p1, ECRITURE);
+ 			execlp("ps","ps","ax",(char *)0);	
+		default: 
+			switch(fork()){
+				case -1: 
+				 	perror("fork"); 
+					exit(1);
+				case 0:	
+
+					initPipe(p1, LECTURE);
+					initPipe(p2, ECRITURE);	
+					execlp("grep", "grep", "bash", (char *)0);	
+ 				default:
+ 					close(p1[0]);
+ 					close(p1[1]);
+ 					switch(fork()) {
+ 						case -1: 
+	 						perror("fork");
+	 						exit(1);
+ 						case 0:
+ 							initPipe(p2, LECTURE);
+ 							execlp("wc", "wc", "-l", (char *)0);
+ 						default:
+ 							close(p2[0]);
+ 							close(p2[1]);
+ 							while(wait(NULL) != -1);
+ 					}
 	return 0;
 }
